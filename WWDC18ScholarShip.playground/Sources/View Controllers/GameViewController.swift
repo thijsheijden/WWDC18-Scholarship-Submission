e74e3dc85a0e282f3d64e0315614e5b00b70478c
Thijs van der Heijden <hi@thijsheijden.nl>
2018-03-24 18:15:12 +0100

    Got the recognizer working and the model now predicts correctly. Had a hard time getting the model to work because of the .coremlmodelc folder that I had to grab from another project

--- WWDC18ScholarShip.playground/Sources/View Controllers/GameViewController.swift
+++ WWDC18ScholarShip.playground/Sources/View Controllers/GameViewController.swift
@@ -1,19 +1,27 @@
 import Foundation
 import CoreML
+import Vision
 import UIKit
 import PlaygroundSupport
 
 public class GameViewController : UIViewController {
     
     let drawView = CanvasView()
+    var request = [VNRequest]()
+    let resultLabel = UILabel()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupCoreMLRequest()
+        
+        print("view loaded")
+        
         //MARK: Variables and constants
         let yellowColor = UIColor(red:1.00, green:0.92, blue:0.23, alpha:1.0)
         var roundNumber = 1
         var currentScore = 0
+        
         
         //MARK: Setting up the UI
         //Setting up the initial view
@@ -21,15 +29,22 @@ public class GameViewController : UIViewController {
         self.view = view
         view.backgroundColor = yellowColor
         
+        //Creating label that shows the result
+        resultLabel.frame = CGRect(x: 187.5, y: 100, width: 100, height: 50)
+        resultLabel.text = "Hello"
+        view.addSubview(resultLabel)
+        
         //Adding the view which can be drawn on
-        drawView.backgroundColor = UIColor.white
+        drawView.backgroundColor = UIColor.black
         drawView.frame = CGRect(x: 12.5, y: 250, width: 350, height: 350)
         drawView.layer.cornerRadius = 20
         view.addSubview(drawView)
         
         //Adding a button to start the round
         let startRoundButton = UIButton()
-        startRoundButton.frame = CGRect(x: 0, y: 50, width: 100, height: 50)
+        let startRoundButtonImage = UIImage(named: "singlePlayerStartButton.png") as UIImage?
+        startRoundButton.setImage(startRoundButtonImage, for: .normal)
+        startRoundButton.frame = CGRect(x: 250, y: 620, width: 100, height: 50)
         view.addSubview(startRoundButton)
         
         startRoundButton.addTarget(self, action: #selector(startRoundButtonPressed), for: .touchUpInside)
@@ -43,12 +58,72 @@ public class GameViewController : UIViewController {
         
         clearCanvasButton.addTarget(self, action: #selector(clearCanvasButtonPressed), for: .touchUpInside)
         
-        //MARK: Functions
+    }
+    
+    //MARK: Functions
+    func setupCoreMLRequest() {
+        let my_model = my_mnist().model
+        
+        print("Coreml setup ran")
+        
+        guard let model = try? VNCoreMLModel(for: my_model) else {
+            fatalError("Cannot load Core ML Model")
+        }
+        
+        // set up request
+        let MNIST_request = VNCoreMLRequest(model: model, completionHandler: handleMNISTClassification)
+        self.request = [MNIST_request]
+    }
+    
+    //Function that handles the classification
+    func handleMNISTClassification(request: VNRequest, error: Error?) {
+        guard let observations = request.results else {
+            debugPrint("No results")
+            return
+        }
+        
+        print(observations)
+        
+        let classification = observations
+            .flatMap({ $0 as? VNClassificationObservation  })
+            .filter({$0.confidence > 0.8})                      // filter confidence > 80%
+            .map({$0.identifier})                               // map the identifier as answer
+        
+        updateLabel(with: classification.first)
         
     }
     
-    @objc func startRoundButtonPressed() {
+    //Functiont that initializes the recognition process and is called every second
+    func recognize() {
+        // The model takes input with 28 by 28 pixels, check the uiimage extension for
+        // - Get snapshot of an view (Canvas)
+        // - Resize image
         
+        print("recognize function ran")
+        
+        let image = UIImage(view: drawView).scale(toSize: CGSize(width: 28, height: 28))
+        
+        let imageRequest = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+        do {
+            try imageRequest.perform(request)
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    //Function that updates the result label
+    func updateLabel(with text: String?) {
+        // update UI should be on main thread
+        DispatchQueue.main.async {
+            self.resultLabel.text = text
+        }
+    }
+    
+    //MARK: Buttons
+    @objc func startRoundButtonPressed() {
+        recognize()
+        print("start button pressed")
     }
     
     //Clear the canvas when the clear button is pressed
