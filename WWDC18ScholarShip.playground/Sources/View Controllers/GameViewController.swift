9aaccdc43cb1228b7fc4ce352856d9675c5aebf7
Thijs van der Heijden <hi@thijsheijden.nl>
2018-03-25 12:57:16 +0200

    Updated the ui, added a popup view that pops up when the user starts the first round, this view will give information about the round they are in. Also made the general UI better and made some small fixes.

--- WWDC18ScholarShip.playground/Sources/View Controllers/GameViewController.swift
+++ WWDC18ScholarShip.playground/Sources/View Controllers/GameViewController.swift
@@ -10,6 +10,12 @@ public class GameViewController : UIViewController {
     var request = [VNRequest]()
     let resultLabel = UILabel()
     let scoreLabel = UILabel()
+    let informationPopUpView = UIView()
+    let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+    var effect:UIVisualEffect!
+    let startRoundButton = UIButton()
+    let clearCanvasButton = UIButton()
+    let closePopUpButton = UIButton()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +30,16 @@ public class GameViewController : UIViewController {
         var currentScore = 0
         
         
+        //Adding the san fransisco black font
+        let cfURL = Bundle.main.url(forResource: "SanFranciscoDisplay-Black", withExtension: "otf")! as CFURL
+        CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
+        var fontNames: [[AnyObject]] = []
+        for name in UIFont.familyNames {
+            fontNames.append(UIFont.fontNames(forFamilyName: name) as [AnyObject])
+        }
+        
         //MARK: Setting up the UI
+        
         //Setting up the initial view
         let view = UIView()
         self.view = view
@@ -36,8 +51,11 @@ public class GameViewController : UIViewController {
         view.addSubview(scoreLabel)
         
         //Creating label that shows what the computer see's
-        resultLabel.frame = CGRect(x: 187.5, y: 100, width: 100, height: 50)
-        resultLabel.text = "Hello"
+        resultLabel.frame = CGRect(x: 37.5, y: 50, width: 300, height: 150)
+        resultLabel.text = "💻 doesn't see anything yet..."
+        resultLabel.font = UIFont(name: "SanFranciscoDisplay-Black", size: 35)
+        resultLabel.textAlignment = .center
+        resultLabel.numberOfLines = 0
         view.addSubview(resultLabel)
         
         //Adding the view which can be drawn on
@@ -46,8 +64,27 @@ public class GameViewController : UIViewController {
         drawView.layer.cornerRadius = 20
         view.addSubview(drawView)
         
+        //MARK: UI for the popup view
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        visualEffectView.effect = nil
+        visualEffectView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+        view.addSubview(visualEffectView)
+        
+        informationPopUpView.layer.cornerRadius = 5
+        informationPopUpView.frame = CGRect(x: 12.5, y: 250, width: 360, height: 300)
+        informationPopUpView.backgroundColor = .white
+        
+        //Adding the close button to the popup view
+        let closePopUpButtonImage = UIImage(named: "closeButton.png") as UIImage?
+        closePopUpButton.setImage(closePopUpButtonImage, for: .normal)
+        closePopUpButton.frame = CGRect(x: 150, y: 230, width: 60, height: 60)
+        informationPopUpView.addSubview(closePopUpButton)
+        
+        closePopUpButton.addTarget(self, action: #selector(closePopUpButtonPressed), for: .touchUpInside)
+        
         //Adding a button to start the round
-        let startRoundButton = UIButton()
         let startRoundButtonImage = UIImage(named: "startButton.png") as UIImage?
         startRoundButton.setImage(startRoundButtonImage, for: .normal)
         startRoundButton.frame = CGRect(x: 252.5, y: 570, width: 110, height: 27)
@@ -56,7 +93,6 @@ public class GameViewController : UIViewController {
         startRoundButton.addTarget(self, action: #selector(startRoundButtonPressed), for: .touchUpInside)
         
         //Adding a button clear the canvas
-        let clearCanvasButton = UIButton()
         let clearCanvasButtonImage = UIImage(named: "clearButton")
         clearCanvasButton.setImage(clearCanvasButtonImage, for: .normal)
         clearCanvasButton.frame = CGRect(x: 12.5, y: 570, width: 110, height: 27)
@@ -122,7 +158,7 @@ public class GameViewController : UIViewController {
     func updateLabel(with text: String?) {
         // update UI should be on main thread
         DispatchQueue.main.async {
-            self.resultLabel.text = text
+            self.resultLabel.text = "I see \(text!)"
         }
     }
     
@@ -135,14 +171,58 @@ public class GameViewController : UIViewController {
     //MARK: Buttons
     @objc func startRoundButtonPressed() {
         //Creating a timer that calls the recognize function every 2 seconds to analyze the drawView
-        var gameTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(GameViewController.startAnalyzingDrawView), userInfo: nil, repeats: true)
-        startAnalyzingDrawView()
+        //var gameTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(GameViewController.startAnalyzingDrawView), userInfo: nil, repeats: true)
+        //startAnalyzingDrawView()
         print("start button pressed")
+        animateIn()
     }
     
     //Clear the canvas when the clear button is pressed
     @objc func clearCanvasButtonPressed() {
         drawView.clearCanvas()
+    }
+    
+    //Close the popup view
+    @objc func closePopUpButtonPressed() {
+        print("close popup pressed")
+        animateOut()
+        UIView.animate(withDuration: 0.15) {
+            self.closePopUpButton.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2.0)
+        }
+    }
+    
+    //MARK: Code to animate in a popup view that shows some of the rules initially (Only in the first round when start is pressed)
+    
+    //Function that animates the appearing of the popup view
+    func animateIn() {
+        self.startRoundButton.isHidden = true
+        self.clearCanvasButton.isHidden = true
+        
+        self.view.addSubview(informationPopUpView)
+        informationPopUpView.center = self.view.center
+        
+        informationPopUpView.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+        informationPopUpView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.visualEffectView.effect = self.effect
+            self.informationPopUpView.alpha = 1
+            self.informationPopUpView.transform = CGAffineTransform.identity
+        }
+        print("popup View added")
+    }
+    
+    //Function that animates the disapearance of the popup view
+    func animateOut () {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.informationPopUpView.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+            self.informationPopUpView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+            
+        }) { (success:Bool) in
+            self.informationPopUpView.removeFromSuperview()
+        }
     }
 
 }
--- WWDC18ScholarShip.playground/Sources/pictionairy.swift
+++ WWDC18ScholarShip.playground/Sources/pictionairy.swift
@@ -10,7 +10,7 @@ import CoreML
 /// Model Prediction Input Type
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 class pictionairyInput : MLFeatureProvider {
-    
+
     /// image (120x120) as color (kCVPixelFormatType_32BGRA) image buffer, 120 pixels wide by 120 pixels high
     var image__120x120_: CVPixelBuffer
     
@@ -36,10 +36,10 @@ class pictionairyInput : MLFeatureProvider {
 /// Model Prediction Output Type
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 class pictionairyOutput : MLFeatureProvider {
-    
+
     /// prediction as dictionary of strings to doubles
     let prediction: [String : Double]
-    
+
     /// classLabel as string value
     let classLabel: String
     
@@ -70,47 +70,46 @@ class pictionairyOutput : MLFeatureProvider {
 @available(macOS 10.13, iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 class pictionairy {
     var model: MLModel
-    
+
     /**
-     Construct a model with explicit path to mlmodel file
-     - parameters:
-     - url: the file url of the model
-     - throws: an NSError object that describes the problem
-     */
+        Construct a model with explicit path to mlmodel file
+        - parameters:
+           - url: the file url of the model
+           - throws: an NSError object that describes the problem
+    */
     init(contentsOf url: URL) throws {
         self.model = try MLModel(contentsOf: url)
     }
-    
+
     /// Construct a model that automatically loads the model from the app's bundle
     convenience init() {
         let bundle = Bundle(for: pictionairy.self)
         let assetPath = bundle.url(forResource: "pictionairy", withExtension:"mlmodelc")
         try! self.init(contentsOf: assetPath!)
     }
-    
+
     /**
-     Make a prediction using the structured interface
-     - parameters:
-     - input: the input to the prediction as pictionairyInput
-     - throws: an NSError object that describes the problem
-     - returns: the result of the prediction as pictionairyOutput
-     */
+        Make a prediction using the structured interface
+        - parameters:
+           - input: the input to the prediction as pictionairyInput
+        - throws: an NSError object that describes the problem
+        - returns: the result of the prediction as pictionairyOutput
+    */
     func prediction(input: pictionairyInput) throws -> pictionairyOutput {
         let outFeatures = try model.prediction(from: input)
         let result = pictionairyOutput(prediction: outFeatures.featureValue(for: "prediction")!.dictionaryValue as! [String : Double], classLabel: outFeatures.featureValue(for: "classLabel")!.stringValue)
         return result
     }
-    
+
     /**
-     Make a prediction using the convenience interface
-     - parameters:
-     - image (120x120) as color (kCVPixelFormatType_32BGRA) image buffer, 120 pixels wide by 120 pixels high
-     - throws: an NSError object that describes the problem
-     - returns: the result of the prediction as pictionairyOutput
-     */
+        Make a prediction using the convenience interface
+        - parameters:
+            - image (120x120) as color (kCVPixelFormatType_32BGRA) image buffer, 120 pixels wide by 120 pixels high
+        - throws: an NSError object that describes the problem
+        - returns: the result of the prediction as pictionairyOutput
+    */
     func prediction(image__120x120_: CVPixelBuffer) throws -> pictionairyOutput {
         let input_ = pictionairyInput(image__120x120_: image__120x120_)
         return try self.prediction(input: input_)
     }
 }
-
